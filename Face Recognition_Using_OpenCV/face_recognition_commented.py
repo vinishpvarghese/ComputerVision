94e4d9ec5a712d272ab053f4ee6c6a3a89318cb3
vinishpvarghese <vinishpvarghese@gmail.com>
2018-09-21 12:11:35 +0530

    Added logic for finding frames per second from video cam

--- Face Recognition_Using_OpenCV/face_recognition_commented.py
+++ Face Recognition_Using_OpenCV/face_recognition_commented.py
@@ -1,6 +1,7 @@
 # Face Recognition using Open CV Library
 
 import cv2
+import time
 
 face_cascade = cv2.CascadeClassifier('haar_frontalface.xml') # We load the cascade for the face.
 eye_cascade = cv2.CascadeClassifier('haar_eye.xml') # We load the cascade for the eyes.
@@ -22,6 +23,57 @@ def detect(gray, frame): # We create a function that takes as input the image in
         #    cv2.rectangle(roi_color,(sx, sy),(sx+sw, sy+sh), (0, 255, 0), 2) # We paint a rectangle around the eyes, but inside the referential of the face.
     return frame # We return the image with the detector rectangles.
 
+
+
+def getfps():
+    # Start default camera
+    video = cv2.VideoCapture(0);
+     
+    # Find OpenCV version
+    (major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+     
+    # With webcam get(CV_CAP_PROP_FPS) does not work.
+    # Let's see for ourselves.
+     
+    if int(major_ver)  < 3 :
+        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+    else :
+        fps = video.get(cv2.CAP_PROP_FPS)
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+     
+ 
+    # Number of frames to capture
+    num_frames = 120;
+     
+    print("Capturing {0} frames".format(num_frames))
+ 
+    # Start time
+    start = time.time()
+     
+    # Grab a few frames
+    for i in range(0, num_frames) :
+        ret, frame = video.read()
+ 
+     
+    # End time
+    end = time.time()
+ 
+    # Time elapsed
+    seconds = end - start
+    print("Time taken : {0} seconds".format(seconds))
+ 
+    # Calculate frames per second
+    fps  = num_frames / seconds;
+    print("Estimated frames per second : {0}".format(fps));
+ 
+    # Release video
+    video.release()
+    
+    return fps
+
+
+fps = getfps() # We find the frames per second 
 video_capture = cv2.VideoCapture(0) # We turn the webcam on.
 
 #For turning on the IP CAM HIK Vision 
@@ -35,6 +87,7 @@ while True: # We repeat infinitely (until break):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # We do some colour transformations.
     canvas = detect(gray, frame) # We get the output of our detect function.
     cv2.imshow('Video', canvas) # We display the outputs.
+    
     if cv2.waitKey(1) & 0xFF == ord('q'): # If we type on the keyboard:
         break # We stop the loop.
 
